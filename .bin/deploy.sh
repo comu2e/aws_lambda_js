1d0f6a4562e6cb3232f744a80735c6c78fe1fca8
comu2e <comu2e@gmail.com>
2022-01-19 13:42:58 +0900

    shellの追加

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -4,7 +4,6 @@ set -euC
 ROLE_NAME=$1
 echo $ROLE_NAME
 zip -r9 deploy_package.zip ./src
-aws iam create-role --role-name $ROLE_NAME --assume-role-policy-document file://./iam/role.json
 ARN=`aws iam list-roles --query "Roles[?RoleName== '$ROLE_NAME'].[RoleName, Arn]" | jq -r '.[][1]'`
 echo $ARN
 aws lambda create-function --role $ARN --function-name $ROLE_NAME --zip-file fileb://deploy_package.zip --handler index.handler --runtime nodejs12.x
