02db51921a9e389ed3cbdb3bce4c543d35b14d66
comu2e <comu2e@gmail.com>
2022-01-19 13:34:16 +0900

    gitignoreの追加

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -1,10 +1,10 @@
-#! /bin/bash
-ROLE_NAME = $1
+#!/bin/bash
+set -euC
 
-zip -r9 deploy_package.zip ./src
-test_s = $ROLE_NAME
-echo $test_s
-aws iam create-role --role-name $ROLE_NAME --assume-role-policy-document file://./iam/role.json
+ROLE_NAME=$1
 echo $ROLE_NAME
-# set ARN 'aws iam list-roles --query "Roles[?RoleName == '$ROLE_NAME'].[RoleName, Arn]" | jq -r '.[][1]' '
+zip -r9 deploy_package.zip ./src
+aws iam create-role --role-name $ROLE_NAME --assume-role-policy-document file://./iam/role.json
+ARN=`aws iam list-roles --query "Roles[?RoleName== '$ROLE_NAME'].[RoleName, Arn]" | jq -r '.[][1]'`
+echo $ARN
 aws lambda create-function --role $ARN --function-name $ROLE_NAME --zip-file fileb://deploy_package.zip --handler index.handler --runtime nodejs12.x
